180e8f886b832e4f8925fbfef4f522adfb08d6c7
olatunji-weber <olatunji.weber@gmail.com>
2022-09-01 17:27:18 +0200

    Another Commit

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import requests
 
 htmlText = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python+Developer&txtLocation=&cboWorkExp1=0').text
 soup = BeautifulSoup(htmlText, 'lxml')
-job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
-companyName = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
-publishedDate = job.find('span', class_ = 'sim-posted').text
+jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+for job in jobs:
+    publishedDate = job.find('span', class_ = 'sim-posted').span.text
+    if 'few' in publishedDate:
+        companyName = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+        skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
 
-print(f''' 
-Company Name: {companyName}
-Required Skills: {skills}
-Published Date: {publishedDate}
-''')
+        print(f''' 
+        Company Name: {companyName}
+        Required Skills: {skills}
+        Published Date: {publishedDate}
+        ''')
+        print()
